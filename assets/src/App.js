97f24f702f44ea6f14714fd982ce8be4263db981
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-08-29 11:58:33 +0200

    BOOKING-142: Cleaned up setup link to drupal

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
-  let arr = ['fisk', 'fusk'];
-
   return (
     <div className="App">
-      {arr.map((el) => (
-        <div>{el}</div>
-      ))}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
