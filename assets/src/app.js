7f0bbe6062d53d2e0d6db9508652c7bb34a6f4d2
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-08-30 12:56:23 +0200

    BOOKING-142: Added comments and example select

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,22 +1,37 @@
 import './app.css';
 import Calendar from "./components/calendar";
 import {useEffect, useState} from "react";
+import Select from "react-select";
 
 function App() {
   const [config, setConfig] = useState(null);
-  const [email, setEmail] = useState('');
-  const [location, setLocation] = useState('DOKK1');
-  const [page, setPage] = useState('fisk');
+  const [location, setLocation] = useState(null);
+  const [availableLocations, setAvailableLocations] = useState([]);
 
   useEffect(() => {
-    setPage("calendar");
-
     if (window?.drupalSettings?.booking_app?.booking) {
       setConfig(window.drupalSettings.booking_app.booking);
 
       console.log("Booking config set", window.drupalSettings.booking_app.booking);
+    } else {
+      // Allow loading from
     }
   }, []);
+
+  useEffect(() => {
+    if (config !== null) {
+      // Fetch locations.
+      setAvailableLocations([
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]);
+    }
+  }, [config]);
+
+  useEffect(() => {
+    // Fetch resources from location.
+  }, [location]);
 
   const onCalendarChange = (param) => {
     console.log("onCalendarChange", param);
@@ -24,16 +39,22 @@ function App() {
 
   return (
     <div className="App">
-      <input value={email} onChange={({target}) => setEmail(target.value)} />
-      <input value={location} onChange={({target}) => setLocation(target.value)} />
+      {/* Add dropdown with options from locations */}
+      {availableLocations.length > 0 &&
+        <Select
+          options={availableLocations}
+        />
+      }
+      {/* Add dropdown with options from resources */}
+      {/* Add dropdown with options from facilities */}
+      {/* Add dropdown with options from capacity */}
 
-      <div>EMAIL: {email}</div>
-      {page === 'calendar' && (
-        <Calendar location={location} onCalendarChange={onCalendarChange} />
-      )}
-      {page === 'fisk' && (
-        <div>FISK</div>
-      )}
+      {/* Add info text box */}
+
+      {/* Display calendar for selections */}
+      <Calendar location={location} onCalendarChange={onCalendarChange} />
+
+      {/* Required author fields */}
     </div>
   );
 }
