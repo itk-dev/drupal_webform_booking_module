83d5e9594a4f8881768fada1349b7c839c19c08e
martinyde <yde001@gmail.com>
2022-09-01 13:00:34 +0200

    Added datepicker

---
+++ assets/src/components/datepicker.js
@@ -0,0 +1,10 @@
+import {useEffect, useState} from "react";
+import Calendar from "./calendar";
+
+function DatePicker({drupalConfig}) {
+  return (
+    <div className="col-md-12"><div className="datepicker pull-right">ABC</div></div>
+  );
+}
+
+export default DatePicker;
