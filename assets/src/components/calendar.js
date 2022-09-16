2bda3c739560898f011072d6af890934feae68d1
Jeppe Krogh <kjej@aarhus.dk>
2022-09-16 12:39:49 +0200

    fixed coding standards

--- assets/src/components/calendar.js
+++ assets/src/components/calendar.js
@@ -131,85 +131,84 @@ function Calendar({
 
   return (
     <div className="Calendar no-gutter col-md-12">
-          <CalendarHeader config={config} date={date} setDate={setDate} />
-          <div className="row">
-            <div className="col-md-12">
+      <CalendarHeader config={config} date={date} setDate={setDate} />
+      <div className="row">
+        <div className="col-md-12">
+          <FullCalendar
+            ref={calendarRef}
+            plugins={[
+              resourceTimegrid,
+              interactionPlugin,
+              dayGridPlugin,
+              timeGridPlugin,
+              listPlugin,
+              resourceTimelinePlugin,
+            ]}
+            titleFormat={{
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }}
+            headerToolbar=""
+            height="650px"
+            scrollTime={getScrollTime()}
+            initialView="resourceTimelineDay"
+            duration="days: 3"
+            selectConstraint="businessHours"
+            selectMirror
+            displayEventTime
+            scrollTimeReset={false}
+            slotLabelFormat={{
+              hour: "numeric",
+              omitZeroMinute: false,
+            }}
+            nowIndicator
+            navLinks
+            slotDuration="00:15:00"
+            allDaySlot={false}
+            resourcesInitiallyExpanded={false}
+            selectable
+            unselectAuto={false}
+            schedulerLicenseKey={config.license_key}
+            slotMinTime="07:00:00"
+            slotMaxTime="21:00:00"
+            selectOverlap={false}
+            nextDayThreshold="21:00:00"
+            editable={false}
+            dayMaxEvents
+            locale={daLocale}
+            select={onCalendarSelection}
+            validRange={getValidRange}
+            resources={
+              resources &&
+              resources.map((value) => handleResources(value, calendarRef))
+            }
+            resourceGroupField="building"
+            resourceAreaColumns={[
               {
-                <FullCalendar
-                  ref={calendarRef}
-                  plugins={[
-                    resourceTimegrid,
-                    interactionPlugin,
-                    dayGridPlugin,
-                    timeGridPlugin,
-                    listPlugin,
-                    resourceTimelinePlugin,
-                  ]}
-                  titleFormat={{
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  }}
-                  headerToolbar=""
-                  height="650px"
-                  scrollTime={getScrollTime()}
-                  initialView="resourceTimelineDay"
-                  duration="days: 3"
-                  selectConstraint="businessHours"
-                  selectMirror
-                  displayEventTime
-                  scrollTimeReset={false}
-                  slotLabelFormat={{
-                    hour: "numeric",
-                    omitZeroMinute: false,
-                  }}
-                  nowIndicator
-                  navLinks
-                  slotDuration="00:15:00"
-                  allDaySlot={false}
-                  resourcesInitiallyExpanded={false}
-                  selectable
-                  unselectAuto={false}
-                  schedulerLicenseKey={config.license_key}
-                  slotMinTime="07:00:00"
-                  slotMaxTime="21:00:00"
-                  selectOverlap={false}
-                  nextDayThreshold="21:00:00"
-                  editable={false}
-                  dayMaxEvents
-                  locale={daLocale}
-                  select={onCalendarSelection}
-                  validRange={getValidRange}
-                  resources={resources && resources.map((value) =>
-                    handleResources(value, calendarRef)
-                  )}
-                  resourceGroupField="building"
-                  resourceAreaColumns={[
-                    {
-                      headerContent: "Ressourcer",
-                      cellContent(arg) {
-                        return renderCalendarCellInfoButton(
-                          arg.resource.title,
-                          arg.resource.extendedProps.resourceId,
-                          triggerResourceView
-                        );
-                      },
-                    },
-                    {
-                      headerContent: <IconChair />,
-                      headerClassNames: "resource-calendar-capacity-header",
-                      width: "60px",
-                      cellClassNames: "resource-calendar-capacity-value",
-                      cellContent(arg) {
-                        return arg.resource.extendedProps.capacity;
-                      },
-                    },
-                  ]}
-                  events={events && events.map((value) => handleBusyIntervals(value))}
-                />
-              }
-            </div>
-          </div>
+                headerContent: "Ressourcer",
+                cellContent(arg) {
+                  return renderCalendarCellInfoButton(
+                    arg.resource.title,
+                    arg.resource.extendedProps.resourceId,
+                    triggerResourceView
+                  );
+                },
+              },
+              {
+                headerContent: <IconChair />,
+                headerClassNames: "resource-calendar-capacity-header",
+                width: "60px",
+                cellClassNames: "resource-calendar-capacity-value",
+                cellContent(arg) {
+                  return arg.resource.extendedProps.capacity;
+                },
+              },
+            ]}
+            events={events && events.map((value) => handleBusyIntervals(value))}
+          />
+        </div>
+      </div>
     </div>
   );
 }
