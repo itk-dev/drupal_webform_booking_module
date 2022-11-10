578b5975fcde449eb5111e3c606bdd5e1a01c5c5
Jeppe Krogh <kjej@aarhus.dk>
2022-11-10 15:34:14 +0100

    coding standards

--- assets/src/components/list-container.js
+++ assets/src/components/list-container.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as PropTypes from "prop-types";
 import List from "./list";
 import NoResultOverlay from "./no-result-overlay";
@@ -11,11 +11,18 @@ import "./list-container.scss";
  * @param {object} props.setShowResourceDetails Resource object to show details for
  * @param {boolean} props.userHasInteracted Has the user interacted with filters
  * @param {boolean} props.isLoading Loading state.
+ * @param {boolean} props.setIsLoading Set loading state.
  * @returns {JSX.Element} List element containing resources
  */
-function ListContainer({ resources, setShowResourceDetails, userHasInteracted, isLoading }) {
+function ListContainer({ resources, setShowResourceDetails, userHasInteracted, isLoading, setIsLoading }) {
+  useEffect(() => {
+    if (resources.length > 0) {
+      setIsLoading(false);
+    }
+  }, [resources]);
+
   return (
-    <div className={`List no-gutter col-md-12 ${(isLoading === true ? "list-loading":"")}`}>
+    <div className={`List no-gutter col-md-12 ${isLoading === true ? "list-loading" : ""}`}>
       {isLoading && <LoadingSpinner />}
       {(!resources || (resources && resources.length === 0)) && !userHasInteracted && (
         <NoResultOverlay state="initial" />
@@ -37,6 +44,7 @@ ListContainer.propTypes = {
   setShowResourceDetails: PropTypes.func.isRequired,
   userHasInteracted: PropTypes.bool,
   isLoading: PropTypes.bool.isRequired,
+  setIsLoading: PropTypes.func.isRequired,
 };
 
 ListContainer.defaultProps = {
--- assets/src/create-booking.js
+++ assets/src/create-booking.js
@@ -212,7 +212,9 @@ function CreateBooking({ config }) {
                       <ListContainer
                         resources={resources}
                         setShowResourceDetails={setShowResourceDetails}
+                        userHasInteracted={userHasInteracted}
                         isLoading={isLoading}
+                        setIsLoading={setIsLoading}
                       />
                       <ResourceView
                         showResourceDetails={showResourceDetails}
