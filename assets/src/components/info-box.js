9a049d3fee200c259289582d1b805afa863ce7e8
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-11-11 10:01:47 +0100

    BOOKING-201: Applied coding standards

--- assets/src/components/info-box.js
+++ assets/src/components/info-box.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import * as PropTypes from "prop-types";
 import "./info-box.scss";
 
@@ -15,20 +15,22 @@ function InfoBox({ config }) {
   const infoBoxHeader = config.info_box_header;
   const infoBoxContent = config.info_box_content;
 
-  return display && (
-    <div className="info-box-wrapper">
-      <div className="row info-box" style={{ backgroundColor: `${infoBoxColor}em` }}>
-        <div className="col-md-11 info-box-content">
-      <span className="info-box-content-header">
-        <b>{infoBoxHeader}</b>
-      </span>
-          <span className="info-box-content-text">{infoBoxContent}</span>
-        </div>
-        <div className="col-md-1 info-box-close" onClick={() => setDisplay(false)} role="presentation">
-          <span>x</span>
+  return (
+    display && (
+      <div className="info-box-wrapper">
+        <div className="row info-box" style={{ backgroundColor: `${infoBoxColor}em` }}>
+          <div className="col-md-11 info-box-content">
+            <span className="info-box-content-header">
+              <b>{infoBoxHeader}</b>
+            </span>
+            <span className="info-box-content-text">{infoBoxContent}</span>
+          </div>
+          <div className="col-md-1 info-box-close" onClick={() => setDisplay(false)} role="presentation">
+            <span>x</span>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
